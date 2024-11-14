06d8c222178748a25ca5b1387418b0ad2cd77b86
AquaPirot <acaglobus@gmail.com>
2024-11-14 10:03:35 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import DailySalesApp from '@/components/DailySalesApp';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <DailySalesApp />
+    </main>
+  );
+}
